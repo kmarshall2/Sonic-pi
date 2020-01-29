50e548d0a90958c97bad20837a164455867b9b44
kmarshall2 <45017660+kmarshall2@users.noreply.github.com>
2020-01-29 09:01:24 -0800

    Add files via upload

---
+++ justcustomsong.rb
@@ -0,0 +1,132 @@
+# Welcome to Sonic Pi v3.1
+use_synth :piano
+
+live_loop :jingle do
+  play :d4
+  play :b3
+  play :g3
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  play :d4
+  play :b3
+  play :g3
+  sleep 2
+  
+  
+  play :d4
+  play :b3
+  play :g3
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  play :e4
+  play :c4
+  play :g3
+  sleep 2
+  
+  
+  play :e4
+  play :c4
+  play :g3
+  sleep 0.5
+  play :c5
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  play :f4
+  play :d4
+  play :a3
+  sleep 0.5
+  
+  play :d5
+  play :d4
+  play :a3
+  sleep 0.5
+  play :d5
+  sleep 0.5
+  play :c5
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :b4
+  play :g4
+  play :b3
+  sleep 2
+  
+  
+  play :d4
+  play :b3
+  play :g3
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  play :d4
+  play :b3
+  play :g3
+  sleep 2
+  
+  
+  play :d4
+  play :b3
+  play :g3
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  play :e4
+  play :c4
+  play :g3
+  sleep 2
+  
+  
+  play :d4
+  play :c4
+  play :g3
+  sleep 0.5
+  play :c5
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :d5
+  play :d4
+  play :g3
+  sleep 1
+  
+  
+  play :e5
+  play :d4
+  play :a3
+  sleep 0.5
+  play :d5
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :a4
+  sleep 0.5
+  play :d5
+  sleep 0.5
+  play :c4
+  play :d4
+  play :d4
+  sleep 1
+end

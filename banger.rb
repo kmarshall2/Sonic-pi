f34fe514163111364e51690a158491af5492f4a7
kmarshall2 <45017660+kmarshall2@users.noreply.github.com>
2020-01-31 10:39:24 -0800

    Add files via upload

---
+++ banger.rb
@@ -0,0 +1,22 @@
+# Welcome to Sonic Pi v3.1
+x=1
+
+banger = "C:/Users/kyle_marshall/Downloads/challenge_f/this_is_a_banger.wav"
+
+
+sample banger
+sleep 3
+
+4.times do
+  print "decrease the rate by 0.1"
+  sample banger, rate: x
+  sleep 3
+  x = x-0.1
+end
+
+4.times do
+  print "increase the rate by 0.1"
+  sample banger, rate: x
+  sleep 3
+  x = x+0.1
+end
